bbcb2f9d3c041ba2ec2b324b4a0ef68f89e43486
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-09-08 10:53:58 +0500

    ...

--- blog/views.py
+++ blog/views.py
@@ -1,70 +1,31 @@
-from django.shortcuts import render
-from datetime import  date
-# Create your views here.
-
-all_posts =[
-    {
-        'slug': "hikeinmountains",
-        'author':'Azamat ',
-        "date": date(2021, 7, 15),
-        'title': 'Mountain haiking',
-        'content': """
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-   
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-    
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-        """
-    },
-    {
-        'slug': "hike-in-mountains",
-        'author':'Azamat ',
-        "date": date(2021, 7, 15),
-        'title': 'Mountain haiking',
-        'content': """
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-   
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-    
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-        """   
-    },
-    {
-        'slug': "hike-in-mountains",
-        'author':'Azamat ',
-        "date": date(2021, 7, 15),
-        'title': 'Mountain haiking',
-        'content': """
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-   
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-    
-             Fuga esse voluptatem perspiciatis. Ex eligendi aperiam molestiae fugit dicta esse nam itaque, iure facilis vel placeat reprehenderit, laudantium saepe beatae, doloremque nulla. Dignissimos doloremque magni est suscipit omnis veritatis?
-        """
-    }
-]
+from django.shortcuts import render, get_object_or_404
+# from datetime import  date
+from .models import Post
 
 
-def get_date(post):
-    return post['date']
+
+# def get_date(post):
+#     return post['date']
 
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:] 
+    latest_posts =  Post.objects.all().order_by('-date')[:3]
     return render(request,'blog/index.html',{"posts":latest_posts})
 
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html",{
         "all_posts": all_posts
     })
 
 
 def post_detail(request,slug):
-    post_id = next(post for post in all_posts if post['slug']== slug)
+    # post_id = next(post for post in all_posts if post['slug']== slug)
+    # post_id = Post.objects.get(slug=slug)
+    post_id = get_object_or_404(Post, slug=slug)
     return render(request,"blog/post-detail.html",{
         "post": post_id  
     })
